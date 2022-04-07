7a4929eecc2c1013e0268cad1152d78b4c532e53
Josh Bello <Jb230913@live.com>
2022-04-07 21:50:27 +0100

    installed uuid for key attr for list

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Pokemon from './Pokemon';
 import { Routes, Route, Link } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
 export const PokemonList = ({pokemons}) => {
     //console.log(pokemons);
   return (
     <ul>
    {
     pokemons.map(pokemon=>(
-      <li key={pokemon.name}>
+      <li key={uuidv4()} id={uuidv4()}>
       {/* <Link to={`/pokemon/${pokemon.name}`} element={<Pokemon pokemon={pokemon.name}/>}> */}
       <Link 
       to={{ pathname: `/pokemon/${pokemon.name}`, query: { pokemon: pokemon.name }}}  element={<Pokemon pokemon={pokemon.name} />}>
